8800cceaf406ad5fc0576086359dfa7b82ea0a9c
HLMC <hl2278535805@outlook.com>
2024-11-02 22:53:04 +0800

    Revert "Update ConfigView.vue"
    This reverts commit 100ae1ad525ac6a1dcfb070c9fbd74e415dce7a0.

--- src/components/ConfigView.vue
+++ src/components/ConfigView.vue
@@ -504,9 +504,9 @@ async function replacePreset() {
         </v-col>
       </v-row>
     </div>
-    <div class="mt-2 px-6">
+    <div class="mt-2">
       <StickyLabel :title="t('title.player')"></StickyLabel>
-      <v-row no-gutters class="mx-n2">
+      <v-row no-gutters class="mx-n2 px-6">
         <v-col cols="4">
           <v-text-field
             readonly
@@ -522,7 +522,7 @@ async function replacePreset() {
           <v-text-field class="mx-2" :label="t('player-name')" v-model="playerName"></v-text-field>
         </v-col>
       </v-row>
-      <v-row no-gutters class="mx-n2">
+      <v-row no-gutters class="mx-n2 px-6">
         <v-col cols="4">
           <v-text-field class="mx-2" :label="t('player-rks')" :rules="[RULES.positive]" type="number" v-model="playerRks"></v-text-field>
         </v-col>
@@ -535,9 +535,9 @@ async function replacePreset() {
       </v-row>
     </div>
 
-    <div class="mt-2 px-6">
+    <div class="mt-2">
       <StickyLabel :title="t('title.graphics')"></StickyLabel>
-      <v-row no-gutters class="mx-n2 align-center">
+      <v-row no-gutters class="mx-n2 align-center px-6">
         <v-col cols="8">
           <v-combobox class="mx-2" :label="t('respack')" :items="respacks" item-title="name" v-model="respack"></v-combobox>
         </v-col>
